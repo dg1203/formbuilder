2ae879e191bd9540bac81605cee6debc3cbccb58
Dominik Gębski <domgebski@gmail.com>
2018-07-16 23:57:42 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    form = [];
+
+    constructor() {}
+  
+    ngOnInit() {
+    }
+  
+    addInput() {
+      const newInput = {
+        question: '',
+        type: '',
+        subInputs: []
+      }
+      this.form.push(newInput);
+      //console.log(this.form)
+    }
+
+    addSubInput(i) {
+      const newSubInput = {
+        question: '',
+        type: '',
+        equals: '',
+        condition: '',
+        subInputs: []
+      }
+      this.form[i].subInputs.push(newSubInput);
+      //console.log(this.form)
+    }
+
+    isRadio(type) {
+      alert(type)
+    }
+
 }
